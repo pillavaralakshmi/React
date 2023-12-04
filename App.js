2341468acd2c310ccedc8216249f39146c39b560
pillavaralakshmi <147524495+pillavaralakshmi@users.noreply.github.com>
2023-12-04 18:46:55 +0530

    Update App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+var jsxheading =(
+  <div>
+  <h1 id ="jsx heading">Heading from JSX!!</h1>
+  <h1 id ="jsx heading">Heading from JSX!!</h1>
+  <h1>Hello, World</h1>
+  <p> Welocme to react <p>
+  </div>
+);
+
+  
